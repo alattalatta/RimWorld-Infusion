3fbb6bd5c5df7d102c58b4fa36be39cc66de4fcd
urty5656 <urty5656@gmail.com>
2015-05-06 17:52:48 +0900

    Separate InfusionSet and StatMod

---
+++ RimWorld-Infusion/Util/InfusionSet.cs
@@ -0,0 +1,41 @@
+using System;
+using Verse;
+
+namespace Infusion
+{
+	public class InfusionSet : IEquatable<InfusionSet>, Saveable
+	{
+		public bool Equals(InfusionSet other)
+		{
+			if (ReferenceEquals(null, other)) return false;
+			if (ReferenceEquals(this, other)) return true;
+			return string.Equals(Prefix, other.Prefix) && string.Equals(Suffix, other.Suffix);
+		}
+
+		public void ExposeData()
+		{
+			Scribe_Values.LookValue(ref Prefix, "Prefix", null);
+			Scribe_Values.LookValue(ref Suffix, "Suffix", null);
+		}
+
+		public string Prefix;
+		public string Suffix;
+
+		public bool PassPre { get { return Prefix == null; } }
+		public bool PassSuf { get { return Suffix == null; } }
+
+		public InfusionSet(string pre, string suf)
+		{
+			Prefix = pre;
+			Suffix = suf;
+		}
+
+		public static InfusionSet Empty
+		{
+			get
+			{
+				return new InfusionSet(null, null);
+			}
+		}
+	}
+}
