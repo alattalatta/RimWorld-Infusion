a5c1f92aa54f7afac65a401f3db229ba3ea8e87b
urty5656 <urty5656@gmail.com>
2015-08-22 20:45:17 +0900

    Minor change

---
+++ RimWorld-Infusion/Infusions/ApparelWithInfusions.cs
@@ -0,0 +1,24 @@
+﻿using Infusion;
+using RimWorld;
+using Verse;
+
+namespace Infusion
+{
+	class ApparelWithInfusions : Apparel
+	{
+		public override string LabelBase
+		{
+			get
+			{
+				QualityCategory qc;
+				if (!this.TryGetQuality(out qc) ||
+					qc < QualityCategory.Good ||
+					this.TryGetComp<CompInfusion>() == null ||
+					!this.TryGetComp<CompInfusion>().Infused)
+					return base.LabelBase;
+
+				return this.GetInfusedLabel();
+			}
+		}
+	}
+}
